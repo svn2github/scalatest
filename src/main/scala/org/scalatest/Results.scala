e517c00a55de0d092d439b0d3b31503bde8c4c15
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-12-31 21:17:48 +0000

    init ver
    git-svn-id: http://scalatest.googlecode.com/svn/trunk@4751 fd1274fd-080f-5963-8033-43ee40fcd1de

---
+++ src/main/scala/org/scalatest/Results.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2001-2012 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+import exceptions.TestCanceledException
+import exceptions.TestOmittedException
+import exceptions.TestOmittedException
+
+trait Results {
+  trait FailedOrCanceled
+  sealed abstract class Result {
+    def isSucceeded: Boolean = false
+    def isFailed: Boolean = false
+    def isCanceled: Boolean = false
+    def isPending: Boolean = false
+    def isOmitted: Boolean = false
+    def isDefined: Boolean = true
+    def isEmpty: Boolean = false
+    def toOption: Option[Throwable] = None
+  }
+  abstract class Exceptional(ex: Throwable) extends Result {
+    override def toOption: Option[Throwable] = Some(ex)
+  }
+  case object Succeeded extends Result {
+    override def isSucceeded: Boolean = true
+    override def isDefined: Boolean = false
+    override def isEmpty: Boolean = true
+  }
+  case class Failed(ex: Throwable) extends Exceptional(ex) with FailedOrCanceled {
+    override def isFailed: Boolean = true
+  }
+  case class Canceled(ex: TestCanceledException) extends Exceptional(ex) with FailedOrCanceled {
+    override def isCanceled: Boolean = true
+  }
+  case class Pending(ex: TestPendingException) extends Exceptional(ex) {
+    override def isPending: Boolean = true
+  }
+  case class Omitted(ex: TestOmittedException) extends Exceptional(ex) {
+    override def isOmitted: Boolean = true
+  }
+  object FailedOrCanceled {
+    def unapply(res: Result): Option[Throwable] = 
+      res match {
+        case Failed(ex) => Some(ex)
+        case Canceled(ex) => Some(ex)
+        case _ => None
+      }
+  }
+  object Exceptional {
+    def unapply(res: Result): Option[Throwable] = 
+      res match {
+        case Failed(ex) => Some(ex)
+        case Canceled(ex) => Some(ex)
+        case Pending(ex) => Some(ex)
+        case Omitted(ex) => Some(ex)
+        case _ => None
+      }
+  }
+  object Result {
+    implicit def convertResultToIterable(res: Result): scala.collection.immutable.Iterable[Throwable] = {
+      res match {
+        case Exceptional(ex) => Vector(ex)
+        case _ => Vector.empty
+      }
+    }
+  }
+
+  def resultOf(f: => Unit): Result = {
+    try {                                         
+      f                                           
+      Succeeded
+    }                                             
+    catch {                                       
+      case ex: TestCanceledException => Canceled(ex)                           
+      case ex: TestPendingException => Pending(ex)                           
+      case ex: TestOmittedException => Omitted(ex)                           
+      case ex: Throwable if !Suite.anErrorThatShouldCauseAnAbort(ex) => Failed(ex)                           
+    }
+  }
+}
+
---
+++ src/test/scala/org/scalatest/ResultsSpec.scala
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2001-2013 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+import exceptions.TestCanceledException
+import exceptions.TestPendingException
+import exceptions.TestOmittedException
+
+class ResultsSpec extends Spec with Results with OptionValues {
+  object `A Result` {
+    def `can be Succeeded` {
+      assert(Succeeded.isSucceeded)
+      assert(!Succeeded.isFailed)
+      assert(!Succeeded.isCanceled)
+      assert(!Succeeded.isPending)
+      assert(!Succeeded.isOmitted)
+    }
+    def `can be Failed` {
+      val ex = new Exception
+      assert(!Failed(ex).isSucceeded)
+      assert(Failed(ex).isFailed)
+      assert(!Failed(ex).isCanceled)
+      assert(!Failed(ex).isPending)
+      assert(!Failed(ex).isOmitted)
+    }
+    def `can be Canceled` {
+      val ex = new TestCanceledException(0)
+      assert(!Canceled(ex).isSucceeded)
+      assert(!Canceled(ex).isFailed)
+      assert(Canceled(ex).isCanceled)
+      assert(!Canceled(ex).isPending)
+      assert(!Canceled(ex).isOmitted)
+    }
+    def `can be Pending` {
+      val ex = new TestPendingException
+      assert(!Pending(ex).isSucceeded)
+      assert(!Pending(ex).isFailed)
+      assert(!Pending(ex).isCanceled)
+      assert(Pending(ex).isPending)
+      assert(!Pending(ex).isOmitted)
+    }
+    def `can be Omitted` {
+      val ex = new TestOmittedException
+      assert(!Omitted(ex).isSucceeded)
+      assert(!Omitted(ex).isFailed)
+      assert(!Omitted(ex).isCanceled)
+      assert(!Omitted(ex).isPending)
+      assert(Omitted(ex).isOmitted)
+    }
+    val res1: Result = Succeeded
+    val ex2 = new Exception
+    val res2: Result = Failed(ex2)
+    val ex3 = new TestCanceledException(0)
+    val res3: Result = Canceled(ex3)
+    val ex4 = new TestPendingException
+    val res4: Result = Pending(ex4)
+    val ex5 = new TestOmittedException
+    val res5: Result = Omitted(ex5)
+    def `can be easily pattern matched on based on whether it is either Failed or Canceled` {
+      def isFailedOrCanceled(res: Result): Boolean =
+        res match {
+          case _: FailedOrCanceled => true
+          case _ => false
+        }
+      assert(!isFailedOrCanceled(res1))
+      assert(isFailedOrCanceled(res2))
+      assert(isFailedOrCanceled(res3))
+      assert(!isFailedOrCanceled(res4))
+      assert(!isFailedOrCanceled(res5))
+    }
+    def `can be easily pattern matched on, extracting the exception, based on whether it is either Failed or Canceled` {
+      def insideFailedOrCanceled(res: Result): Option[Throwable] =
+        res match {
+          case FailedOrCanceled(ex) => Some(ex)
+          case _ => None
+        }
+      assert(insideFailedOrCanceled(res1).isEmpty)
+      assert(insideFailedOrCanceled(res2).value eq ex2)
+      assert(insideFailedOrCanceled(res3).value eq ex3)
+      assert(insideFailedOrCanceled(res4).isEmpty)
+      assert(insideFailedOrCanceled(res5).isEmpty)
+    }
+    def `can be easily pattern matched on based on whether it is Exceptional` {
+      def isExceptional(res: Result): Boolean =
+        res match {
+          case _: Exceptional => true
+          case _ => false
+        }
+      assert(!isExceptional(res1))
+      assert(isExceptional(res2))
+      assert(isExceptional(res3))
+      assert(isExceptional(res4))
+      assert(isExceptional(res5))
+    }
+    def `can be easily pattern matched on, extracting the exception, based on whether it is Exceptional` {
+      def insideExceptional(res: Result): Option[Throwable] =
+        res match {
+          case Exceptional(ex) => Some(ex)
+          case _ => None
+        }
+      assert(insideExceptional(res1).isEmpty)
+      assert(insideExceptional(res2).value eq ex2)
+      assert(insideExceptional(res3).value eq ex3)
+      assert(insideExceptional(res4).value eq ex4)
+      assert(insideExceptional(res5).value eq ex5)
+    }
+    def `can be queried to determine whether or not it is defined` {
+      assert(!res1.isDefined)
+      assert(res2.isDefined)
+      assert(res3.isDefined)
+      assert(res4.isDefined)
+      assert(res5.isDefined)
+    }
+    def `can be queried to determine whether or not it is empty` {
+      assert(res1.isEmpty)
+      assert(!res2.isEmpty)
+      assert(!res3.isEmpty)
+      assert(!res4.isEmpty)
+      assert(!res5.isEmpty)
+    }
+    def `can be transformed into an Option[Throwable]` {
+      assert(res1.toOption.isEmpty)
+      assert(res2.toOption.value eq ex2)
+      assert(res3.toOption.value eq ex3)
+      assert(res4.toOption.value eq ex4)
+      assert(res5.toOption.value eq ex5)
+    }
+    def `can be implicitly converted to an Iterable` {
+      assert(res1.iterator.size === 0)
+      assert(res2.iterator.size === 1)
+      assert(res3.iterator.size === 1)
+      assert(res4.iterator.size === 1)
+      assert(res5.iterator.size === 1)
+      assert(Vector(res1, res2, res3, res4, res5).flatten === Vector(ex2, ex3, ex4, ex5))
+    }
+  }
+  object `The resultOf method` {
+    def `must transform expression evaluations into the appropriate Result class` {
+      assert(resultOf { 99 } == Succeeded)
+      val tfe = new TestFailedException(0)
+      assert(resultOf { throw tfe } === Failed(tfe))
+      val iae = new IllegalArgumentException
+      assert(resultOf { throw iae } === Failed(iae))
+      val tce = new TestCanceledException(0)
+      assert(resultOf { throw tce } === Canceled(tce))
+      val tpe = new TestPendingException
+      assert(resultOf { throw tpe } === Pending(tpe))
+      val toe = new TestOmittedException
+      assert(resultOf { throw toe } === Omitted(toe))
+    }
+    def `if UnknownError is thrown, should complete abruptly with that exception` {
+      intercept[UnknownError] {
+        resultOf { throw new UnknownError }
+      }
+    }
+  }
+}
+
