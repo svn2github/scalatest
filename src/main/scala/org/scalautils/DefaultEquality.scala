ccee566c3307a9ecb66141bfd2a5b28a319931b7
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-01-15 01:17:45 +0000

    Tweaked copyright date on scalautils files. Fleshed out Scaladoc on a few.
    git-svn-id: http://scalatest.googlecode.com/svn/trunk@4827 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalautils/DefaultEquality.scala
+++ src/main/scala/org/scalautils/DefaultEquality.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2012 Artima, Inc.
+ * Copyright 2001-2013 Artima, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,22 @@
  */
 package org.scalautils
 
+/**
+ * A default <code>Equality</code> type class implementation (which can be used for any type) whose
+ * <code>areEqual</code> method compares the passed objects with <code>==</code>, calling <code>.deep</code>
+ * first on any passed object that is an array.
+ * </p>
+ */
 final class DefaultEquality[A] extends Equality[A] {
 
+  /**
+   * Indicates whether the objects passed as <code>a</code> and <code>b</code> are equal by invoking <code>==</code> on <code>a</code>
+   * passing in <code>b</code>, treating arrays specially by invoking <code>.deep</code> on <code>a</code> and/or </code>b</code> if they
+   * are arrays, and using the result or results of invoking <code>.deep</code> in the equality check.
+   *
+   * @param a a left-hand-side object being compared with another (right-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   * @param b a right-hand-side object being compared with another (left-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   */
   def areEqual(a: A, b: Any): Boolean = {
     a match {
       case arr: Array[_] =>
--- src/main/scala/org/scalautils/EqualityConstraint.scala
+++ src/main/scala/org/scalautils/EqualityConstraint.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2012 Artima, Inc.
+ * Copyright 2001-2013 Artima, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,19 +17,85 @@ package org.scalautils
 
 import annotation.implicitNotFound
 
+// I'm unaware of any reason this needs to be sealed, but can start out that way and loosen it if someone
+// wants to create some other kind of constraint.
+
+/**
+ * Abstract class used to enforce type constraints for equality checks.
+ */
 @implicitNotFound(msg = "types ${A} and ${B} do not adhere to the equality constraint selected for the === and !== operators; they must either be in a subtype/supertype relationship, or, if ConversionCheckedTripleEquals is in force, implicitly convertible in one direction or the other; the missing implicit parameter is of type org.scalautils.EqualityConstraint[${A},${B}]")
-abstract class EqualityConstraint[A, B] {
-  def areEqual(left: A, right: B): Boolean
+sealed abstract class EqualityConstraint[A, B] {
+
+  /**
+   * Indicates whether the objects passed as <code>a</code> and <code>b</code> are equal.
+   *
+   * @param a a left-hand-side object being compared with another (right-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   * @param b a right-hand-side object being compared with another (left-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   */
+  def areEqual(a: A, b: B): Boolean
 }
 
-class BasicEqualityConstraint[A, B](equalityOfA: Equality[A]) extends EqualityConstraint[A, B] {
-  def areEqual(left: A, right: B): Boolean = equalityOfA.areEqual(left, right)
+/**
+ * An implementation of <code>EqualityConstraint</code> for two types <code>A</code> and <code>B</code> that requires an <code>Equality[A]</code> to
+ * which its <code>areEqual</code> method can delegate an equality comparison.
+ *
+ * @param equalityofA an <code>Equality</code> type class for <code>A</code>
+ */
+final class BasicEqualityConstraint[A, B](equalityOfA: Equality[A]) extends EqualityConstraint[A, B] {
+
+  /**
+   * Indicates whether the objects passed as <code>a</code> and <code>b</code> are equal by returning the
+   * result of invoking <code>areEqual(a, b)</code> on the passed <code>equalityOfA</code> object.
+   *
+   * @param a a left-hand-side object being compared with another (right-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   * @param b a right-hand-side object being compared with another (left-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   */
+  def areEqual(a: A, b: B): Boolean = equalityOfA.areEqual(a, b)
 }
 
-class BToAEqualityConstraint[A, B](equalityOfA: Equality[A], cnv: B => A) extends EqualityConstraint[A, B] {
-  override def areEqual(left: A, right: B): Boolean = equalityOfA.areEqual(left, cnv(right))
+/**
+ * An implementation of <code>EqualityConstraint</code> for two types <code>A</code> and <code>B</code> that requires an <code>Equality[A]</code>
+ * and a conversion function from <code>B</code> to <code>A</code>. 
+ *
+ * @param equalityofA an <code>Equality</code> type class for <code>A</code>
+ */
+final class BToAEqualityConstraint[A, B](equalityOfA: Equality[A], cnv: B => A) extends EqualityConstraint[A, B] {
+
+  /**
+   * Indicates whether the objects passed as <code>a</code> and <code>b</code> are equal by returning the
+   * result of invoking <code>areEqual(a, cnv(b))</code> on the passed <code>equalityOfA</code> object.
+   *
+   * <p>
+   * In other words, the <code>b</code> object of type <code>B</code> is first converted to an <code>A</code> via the passed conversion
+   * function, <code>cnv</code>, then compared for equality with the <code>a</code> object.
+   * </p>
+   *
+   * @param a a left-hand-side object being compared with another (right-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   * @param b a right-hand-side object being compared with another (left-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   */
+  override def areEqual(a: A, b: B): Boolean = equalityOfA.areEqual(a, cnv(b))
 }
 
-class AToBEqualityConstraint[A, B](equalityOfB: Equality[B], cnv: A => B) extends EqualityConstraint[A, B] {
-  override def areEqual(left: A, right: B): Boolean = equalityOfB.areEqual(cnv(left), right)
+/**
+ * An implementation of <code>EqualityConstraint</code> for two types <code>A</code> and <code>B</code> that requires an <code>Equality[B]</code>
+ * and a conversion function from <code>A</code> to <code>B</code>. 
+ *
+ * @param equalityofB an <code>Equality</code> type class for <code>B</code>
+ */
+final class AToBEqualityConstraint[A, B](equalityOfB: Equality[B], cnv: A => B) extends EqualityConstraint[A, B] {
+
+  /**
+   * Indicates whether the objects passed as <code>a</code> and <code>b</code> are equal by return the
+   * result of invoking <code>areEqual(cnv(a), b)</code> on the passed <code>equalityOfB</code> object.
+   *
+   * <p>
+   * In other words, the <code>a</code> object of type <code>A</code> is first converted to a <code>B</code> via the passed conversion
+   * function, <code>cnv</code>, then compared for equality with the <code>b</code> object.
+   * </p>
+   *
+   * @param a a left-hand-side object being compared with another (right-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   * @param b a right-hand-side object being compared with another (left-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
+   */
+  override def areEqual(a: A, b: B): Boolean = equalityOfB.areEqual(cnv(a), b)
 }
+
