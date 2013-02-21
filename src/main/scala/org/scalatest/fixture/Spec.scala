f1c3db9f8d8d1ea9d6b01417c7e9997a73420f47
george.berger <george.berger@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-02-21 18:32:25 +0000

    added missing fixture.Spec refactor
    git-svn-id: http://scalatest.googlecode.com/svn/trunk@4996 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalatest/fixture/Spec.scala
+++ src/main/scala/org/scalatest/fixture/Spec.scala
@@ -25,31 +25,31 @@ import scala.reflect.NameTransformer._
 import java.lang.reflect.{Method, Modifier, InvocationTargetException}
 
 /**
- * A sister trait to <code>org.scalatest.Spec</code> that can pass a fixture object into its tests.
+ * A sister class to <code>org.scalatest.Spec</code> that can pass a fixture object into its tests.
  *
  * <table><tr><td class="usage">
  * <strong>Recommended Usage</strong>:
- * Use trait <code>fixture.Spec</code> in situations for which <a href="../Spec.html"><code>Spec</code></a>
+ * Use class <code>fixture.Spec</code> in situations for which <a href="../Spec.html"><code>Spec</code></a>
  * would be a good choice, when all or most tests need the same fixture objects
- * that must be cleaned up afterwords. <em>Note: <code>fixture.Spec</code> is intended for use in special situations, with trait <code>Spec</code> used for general needs. For
- * more insight into where <code>fixture.Spec</code> fits in the big picture, see the <a href="../Spec.html#withFixtureOneArgTest"><code>withFixture(OneArgTest)</code></a> subsection of the <a href="../Spec.html#sharedFixtures">Shared fixtures</a> section in the documentation for trait <code>Spec</code>.</em>
+ * that must be cleaned up afterwords. <em>Note: <code>fixture.Spec</code> is intended for use in special situations, with class <code>Spec</code> used for general needs. For
+ * more insight into where <code>fixture.Spec</code> fits in the big picture, see the <a href="../Spec.html#withFixtureOneArgTest"><code>withFixture(OneArgTest)</code></a> subsection of the <a href="../Spec.html#sharedFixtures">Shared fixtures</a> section in the documentation for class <code>Spec</code>.</em>
  * </td></tr></table>
  * 
  * <p>
- * Trait <code>fixture.Spec</code> behaves similarly to trait <code>org.scalatest.Spec</code>, except that tests may have a
+ * Class <code>fixture.Spec</code> behaves similarly to class <code>org.scalatest.Spec</code>, except that tests may have a
  * fixture parameter. The type of the
- * fixture parameter is defined by the abstract <code>FixtureParam</code> type, which is declared as a member of this trait.
- * This trait also declares an abstract <code>withFixture</code> method. This <code>withFixture</code> method
- * takes a <code>OneArgTest</code>, which is a nested trait defined as a member of this trait.
+ * fixture parameter is defined by the abstract <code>FixtureParam</code> type, which is a member of this class.
+ * This class also has an abstract <code>withFixture</code> method. This <code>withFixture</code> method
+ * takes a <code>OneArgTest</code>, which is a nested trait defined as a member of this class.
  * <code>OneArgTest</code> has an <code>apply</code> method that takes a <code>FixtureParam</code>.
  * This <code>apply</code> method is responsible for running a test.
- * This trait's <code>runTest</code> method delegates the actual running of each test to <code>withFixture(OneArgTest)</code>, passing
- * in the test code to run via the <code>OneArgTest</code> argument. The <code>withFixture(OneArgTest)</code> method (abstract in this trait) is responsible
+ * This class's <code>runTest</code> method delegates the actual running of each test to <code>withFixture(OneArgTest)</code>, passing
+ * in the test code to run via the <code>OneArgTest</code> argument. The <code>withFixture(OneArgTest)</code> method (abstract in this class) is responsible
  * for creating the fixture argument and passing it to the test function.
  * </p>
  * 
  * <p>
- * Subclasses of this trait must, therefore, do three things differently from a plain old <code>org.scalatest.Spec</code>:
+ * Subclasses of this class must, therefore, do three things differently from a plain old <code>org.scalatest.Spec</code>:
  * </p>
  * 
  * <ol>
@@ -61,7 +61,7 @@ import java.lang.reflect.{Method, Modifier, InvocationTargetException}
  *
  * <p>
  * If the fixture you want to pass into your tests consists of multiple objects, you will need to combine
- * them into one object to use this trait. One good approach to passing multiple fixture objects is
+ * them into one object to use this class. One good approach to passing multiple fixture objects is
  * to encapsulate them in a case class. Here's an example:
  * </p>
  *
@@ -236,345 +236,7 @@ import java.lang.reflect.{Method, Modifier, InvocationTargetException}
  * @author Bill Venners
  */
 @Finders(Array("org.scalatest.finders.SpecFinder"))
-trait Spec extends Suite  { thisSuite => 
-
-  private final val engine = new FixtureEngine[FixtureParam]("concurrentSpecMod", "Spec")
-  import engine._
-  // Sychronized on thisSuite, only accessed from ensureScopesAndTestsRegistered
-  private var scopesRegistered = false
-  
-  private def ensureScopesAndTestsRegistered() {
-    
-    thisSuite.synchronized {
-      if (!scopesRegistered) {
-        scopesRegistered = true
-        def getMethod(o: AnyRef, testName: String) = { 
-          val methodName = encode(simpleNameForTest(testName))
-          val candidateMethods = o.getClass.getMethods.filter(_.getName == methodName)
-          if (candidateMethods.size == 0)
-            throw new IllegalArgumentException(Resources("testNotFound", testName))
-          candidateMethods(0)
-        }
-        
-        def getMethodTags(o: AnyRef, methodName: String) =
-          for {
-            a <- getMethod(o, methodName).getDeclaredAnnotations
-            annotationClass = a.annotationType
-            if annotationClass.isAnnotationPresent(classOf[TagAnnotation])
-          } yield annotationClass.getName
-          
-        def getScopeClassName(o: AnyRef): String = {
-          val className = o.getClass.getName
-          if (className.endsWith("$"))
-            className 
-          else
-            className + "$"
-        }
-          
-        def isScopeMethod(o: AnyRef, m: Method): Boolean = {
-          val scopeMethodName = getScopeClassName(o)+ m.getName + "$"
-          
-          val returnTypeName = m.getReturnType.getName
-          
-          equalIfRequiredCompactify(scopeMethodName, returnTypeName)
-        }
-        
-        def getScopeDesc(m: Method): String = {
-          val objName = m.getReturnType.getName
-          val objClassName = decode(objName.substring(0, objName.length - 1))
-          objClassName.substring(objClassName.lastIndexOf("$") + 1)
-        }
-        
-        val testTags = tags
-        object MethodNameEncodedOrdering extends Ordering[Method] {
-          def compare(x: Method, y: Method): Int = {
-            decode(x.getName) compareTo decode(y.getName)
-          }
-        }
-        
-        def register(o: AnyRef) {
-          val testMethods = o.getClass.getMethods.filter(isTestMethod(_)).sorted(MethodNameEncodedOrdering)
-          
-// TODO: Detect duplicate test names, one with fixture param and one without.
-          testMethods.foreach { m =>
-            val scope = isScopeMethod(o, m)
-            if (scope) {
-              val scopeDesc = getScopeDesc(m)
-              def scopeFun = {
-                try {
-                  val scopeObj = m.invoke(o)
-                  register(scopeObj)
-                }
-                catch {
-                  case ite: InvocationTargetException if ite.getTargetException != null =>
-                    throw ite.getTargetException
-                }
-              }
-              val scopeLocation = TopOfClass(m.getReturnType.getName)
-              registerNestedBranch(scopeDesc, None, scopeFun, "registrationAlreadyClosed", sourceFileName, "ensureScopesAndTestsRegistered", 2, 0, Some(scopeLocation))
-            }
-            else {
-              val methodName = m.getName
-              val testName = 
-                // if (m.getParameterTypes.length == 0)
-                  decode(methodName)
-                // else
-                  // decode(methodName) + FixtureInParens
-              val methodTags = getMethodTags(o, testName)
-              val testFun: FixtureParam => Unit = (fixture: FixtureParam) => { 
-                val anyRefFixture: AnyRef = fixture.asInstanceOf[AnyRef] // TODO zap this cast
-                val argsArray: Array[Object] = 
-                  if (m.getParameterTypes.length == 0)
-                    Array.empty
-                  else
-                    Array(anyRefFixture)  
-                try m.invoke(o, argsArray: _*)
-                catch {
-                  case ite: InvocationTargetException => 
-                    throw ite.getTargetException
-                }
-              }
-          
-              val testLocation = TopOfMethod(getScopeClassName(o), m.toGenericString)
-              val isIgnore = testTags.get(methodName) match {
-                case Some(tagSet) => tagSet.contains(IgnoreAnnotation) || methodTags.contains(IgnoreAnnotation)
-                case None => methodTags.contains(IgnoreAnnotation)
-              }
-              if (isIgnore)
-                registerIgnoredTest(testName, testFun, "registrationAlreadyClosed", sourceFileName, "ensureScopesAndTestsRegistered", 3, 0, Some(testLocation), methodTags.map(new Tag(_)): _*)
-              else
-                registerTest(testName, testFun, "registrationAlreadyClosed", sourceFileName, "ensureScopesAndTestsRegistered", 2, 1, None, Some(testLocation), None, methodTags.map(new Tag(_)): _*)
-            }
-          }
-        }
-     
-        register(thisSuite)
-      }
-    }
-  }
-
-  // TODO: Probably make this private final val sourceFileName in a singleton object so it gets compiled in rather than carried around in each instance
-  private[scalatest] val sourceFileName = "Spec.scala"
-
-  /**
-   * Returns an <code>Informer</code> that during test execution will forward strings (and other objects) passed to its
-   * <code>apply</code> method to the current reporter. If invoked in a constructor, it
-   * will register the passed string for forwarding later during test execution. If invoked while this
-   * <code>Spec</code> is being executed, such as from inside a test function, it will forward the information to
-   * the current reporter immediately. If invoked at any other time, it will
-   * throw an exception. This method can be called safely by any thread.
-   */
-  implicit protected def info: Informer = atomicInformer.get
-  
-  /**
-   * Returns a <code>Documenter</code> that during test execution will forward strings passed to its
-   * <code>apply</code> method to the current reporter. If invoked in a constructor, it
-   * will register the passed string for forwarding later during test execution. If invoked while this
-   * <code>Spec</code> is being executed, such as from inside a test function, it will forward the information to
-   * the current reporter immediately. If invoked at any other time, it will
-   * throw an exception. This method can be called safely by any thread.
-   */
-  implicit protected def markup: Documenter = atomicDocumenter.get
-  
-  /**
-   * An immutable <code>Set</code> of test names. If this <code>Spec</code> contains no tests, this method returns an
-   * empty <code>Set</code>.
-   *
-   * <p>
-   * This trait's implementation of this method will return a set that contains the names of all registered tests. The set's
-   * iterator will return those names in the order in which the tests were registered. Each test's name is composed
-   * of the concatenation of the name of each surrounding <em>scope object</em>, in order from outside in, and the name of the
-   * test method itself, with all components separated by a space. For example, consider this <code>Spec</code>:
-   * </p>
-   *
-   * <pre class="stHighlight">
-   * import org.scalatest.Spec
-   *
-   * class StackSpec extends Spec {
-   *   object &#96;A Stack&#96; {
-   *     object &#96;(when not empty)&#96; {
-   *       def &#96;must allow me to pop&#96; {}
-   *     }
-   *     object &#96;(when not full)&#96; {
-   *       def &#96;must allow me to push&#96; {}
-   *     }
-   *   }
-   * }
-   * </pre>
-   *
-   * <p>
-   * Invoking <code>testNames</code> on this <code>Spec</code> will yield a set that contains the following
-   * two test name strings:
-   * </p>
-   *
-   * <pre class="stExamples">
-   * "A Stack (when not empty) must allow me to pop"
-   * "A Stack (when not full) must allow me to push"
-   * </pre>
-   *
-   * <p>
-   * This trait's implementation of this method will first ensure that the discovery of scope objects and test methods
-   * has been performed.
-   * </p>
-   */
-  override def testNames: Set[String] = {
-    ensureScopesAndTestsRegistered()
-    // I'm returning a ListSet here so that they tests will be run in registration order
-    ListSet(atomic.get.testNamesList.toArray: _*)
-  }
-  
-  /**
-   * Run a test. This trait's implementation runs the test registered with the name specified by
-   * <code>testName</code>. Each test's name is a concatenation of the text of all <em>scope objects</em> surrounding a test,
-   * from outside in, and the test method's name, with one space placed between each item. (See the documentation
-   * for <code>testNames</code> for an example.)
-   *
-   * <p>
-   * This trait's implementation of this method will first ensure that the discovery of scope objects and test methods
-   * has been performed.
-   * </p>
-   *
-   * @param testName the name of one test to execute.
-   * @param args the <code>Args</code> for this run
-   *
-   * @throws NullPointerException if any of <code>testName</code>, <code>reporter</code>, <code>stopper</code>, or <code>configMap</code>
-   *     is <code>null</code>.
-   */
-  protected override def runTest(testName: String, args: Args): Status = {
-
-    ensureScopesAndTestsRegistered()
-
-    def invokeWithFixture(theTest: TestLeaf) {
-      val theConfigMap = args.configMap
-      val testData = testDataFor(testName, theConfigMap)
-      withFixture(
-        new OneArgTest {
-          val name = testData.name
-          def apply(fixture: FixtureParam) { theTest.testFun(fixture) }
-          val configMap = testData.configMap
-          val scopes = testData.scopes
-          val text = testData.text
-          val tags = testData.tags
-        }
-        //new TestFunAndConfigMap(testName, theTest.testFun, theConfigMap)
-      )
-    }
-
-    runTestImpl(thisSuite, testName, args, true, invokeWithFixture)
-  }
-  
-  /**
-   * The total number of tests that are expected to run when this <code>Spec</code>'s <code>run</code> method is invoked.
-   *
-   * <p>
-   * This trait's implementation of this method returns the sum of:
-   * </p>
-   *
-   * <ul>
-   * <li>the size of the <code>testNames</code> <code>List</code>, minus the number of tests marked as ignored and
-   * any tests that are exluded by the passed <code>Filter</code></li>
-   * <li>the sum of the values obtained by invoking
-   *     <code>expectedTestCount</code> on every nested <code>Suite</code> contained in
-   *     <code>nestedSuites</code></li>
-   * </ul>
-   *
-   * <p>
-   * This trait's implementation of this method will first ensure that the discovery of scope objects and test methods
-   * has been performed.
-   * </p>
-   *
-   * @param filter a <code>Filter</code> with which to filter tests to count based on their tags
-   */
-  final override def expectedTestCount(filter: Filter): Int = {
-    ensureScopesAndTestsRegistered()
-    super.expectedTestCount(filter)
-  }
-
-  /**
-   * A <code>Map</code> whose keys are <code>String</code> tag names to which tests in this <code>Spec</code> belong, and values
-   * the <code>Set</code> of test names that belong to each tag. If this <code>Spec</code> contains no tags, this method returns an empty <code>Map</code>.
-   *
-   * <p>
-   * This trait's implementation returns tags that were passed as strings contained in <code>Tag</code> objects passed to 
-   * methods <code>test</code> and <code>ignore</code>. 
-   * </p>
-   * 
-   * <p>
-   * In addition, this trait's implementation will also auto-tag tests with class level annotations.  
-   * For example, if you annotate @Ignore at the class level, all test methods in the class will be auto-annotated with @Ignore.
-   * </p>
-   *
-   * <p>
-   * This trait's implementation of this method will first ensure that the discovery of scope objects and test methods
-   * has been performed.
-   * </p>
-   */
-  override def tags: Map[String, Set[String]] = {
-    ensureScopesAndTestsRegistered()
-    autoTagClassAnnotations(atomic.get.tagsMap, this)
-  }
-
-  /**
-   * Run zero to many of this <code>Spec</code>'s tests.
-   *
-   * @param testName an optional name of one test to run. If <code>None</code>, all relevant tests should be run.
-   *                 I.e., <code>None</code> acts like a wildcard that means run all relevant tests in this <code>Spec</code>.
-   * @param args the <code>Args</code> for this run
-   *
-   * @throws NullPointerException if any of the passed parameters is <code>null</code>.
-   * @throws IllegalArgumentException if <code>testName</code> is defined, but no test with the specified test name
-   *     exists in this <code>Spec</code>
-   */
-  protected override def runTests(testName: Option[String], args: Args): Status = {
-    ensureScopesAndTestsRegistered()
-    runTestsImpl(thisSuite, testName, args, info, true, runTest)
-  }
-
-  /**
-   * Runs this <code>fixture.Spec</code>.
-   *
-   * <p>If <code>testName</code> is <code>None</code>, this trait's implementation of this method
-   * calls these two methods on this object in this order:</p>
-   *
-   * <ol>
-   * <li><code>runNestedSuites(report, stopper, tagsToInclude, tagsToExclude, configMap, distributor)</code></li>
-   * <li><code>runTests(testName, report, stopper, tagsToInclude, tagsToExclude, configMap)</code></li>
-   * </ol>
-   *
-   * <p>
-   * If <code>testName</code> is defined, then this trait's implementation of this method
-   * calls <code>runTests</code>, but does not call <code>runNestedSuites</code>. This behavior
-   * is part of the contract of this method. Subclasses that override <code>run</code> must take
-   * care not to call <code>runNestedSuites</code> if <code>testName</code> is defined. (The
-   * <code>OneInstancePerTest</code> trait depends on this behavior, for example.)
-   * </p>
-   *
-   * <p>
-   * This trait's implementation of this method will first ensure that the discovery of scope objects and test methods
-   * has been performed.
-   * </p>
-   *
-   *
-   * @param testName an optional name of one test to run. If <code>None</code>, all relevant tests should be run.
-   *                 I.e., <code>None</code> acts like a wildcard that means run all relevant tests in this <code>Suite</code>.
-   * @param args the <code>Args</code> for this run
-   *         
-   * @throws NullPointerException if any passed parameter is <code>null</code>.
-   * @throws IllegalArgumentException if <code>testName</code> is defined, but no test with the specified test name
-   *     exists in this <code>Suite</code>
-   */
-  override def run(testName: Option[String], args: Args): Status = {
-    ensureScopesAndTestsRegistered()
-    runImpl(thisSuite, testName, args, super.run)
-  }
-  
-  /**
-   * Suite style name.
-   */
-  final override val styleName: String = "org.scalatest.fixture.Spec"
-
-  override def testDataFor(testName: String, theConfigMap: ConfigMap = ConfigMap.empty): TestData = createTestDataFor(testName, theConfigMap, this)
-}
+abstract class Spec extends SpecLike
 
 private[scalatest] object Spec {
   
