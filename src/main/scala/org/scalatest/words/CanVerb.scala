7a334edbb0a9f2106b1529ec435da97a77e8edd6
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-02-26 19:23:15 +0000

    Moved DSL support things in verbs package to words packages.
    git-svn-id: http://scalatest.googlecode.com/svn/trunk@5049 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalatest/words/CanVerb.scala
+++ src/main/scala/org/scalatest/words/CanVerb.scala
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatest.verb
+package org.scalatest.words
 
 import org.scalatest._
 
 /**
- * Provides an implicit conversion that adds <code>should</code> methods to <code>String</code>
- * to support the syntax of <code>FlatSpec</code>, <code>WordSpec</code>, <code>fixture.FlatSpec</code>,
+ * Provides an implicit conversion that adds <code>can</code> methods to <code>String</code>
+ * to support the syntax of <code>FlatSpec</code>, <code>WordSpec</code>, <code>org.scalatest.fixture.FlatSpec</code>,
  * and <code>fixture.WordSpec</code>.
  *
  * <p>
@@ -28,7 +28,7 @@ import org.scalatest._
  * </p>
  *
  * <pre class="stHighlight">
- * "A Stack (when empty)" should "be empty" in { ... }
+ * "A Stack (when empty)" can "be empty" in { ... }
  *                        ^
  * </pre>
  *
@@ -38,7 +38,7 @@ import org.scalatest._
  * </p>
  *
  * <pre class="stHighlight">
- * "A Stack (with one item)" should behave like nonEmptyStack(stackWithOneItem, lastValuePushed)
+ * "A Stack (with one item)" can behave like nonEmptyStack(stackWithOneItem, lastValuePushed)
  *                           ^
  * </pre>
  *
@@ -48,7 +48,7 @@ import org.scalatest._
  * </p>
  *
  * <pre class="stHighlight">
- * "A Stack (when empty)" should { ...
+ * "A Stack (when empty)" can { ...
  *                        ^
  * </pre>
  *
@@ -60,61 +60,37 @@ import org.scalatest._
  * <pre class="stHighlight">
  *    def provide = afterWord("provide")
  *
- *   "The ScalaTest Matchers DSL" should provide {
+ *   "The ScalaTest Matchers DSL" can provide {
  *                                ^
  * </pre>
  *
  * <p>
  * The reason this implicit conversion is provided in a separate trait, instead of being provided
  * directly in <code>FlatSpec</code>, <code>WordSpec</code>, <code>fixture.FlatSpec</code>, and
- * <code>fixture.WordSpec</code>, is because an implicit conversion provided directly would conflict
- * with the implicit conversion that provides <code>should</code> methods on <code>String</code>
- * in the <code>ShouldMatchers</code> trait. By contrast, there is no conflict with
- * the separate <code>ShouldVerb</code> trait approach, because:
- * </p>
- *
- * <ol>
- * <li><code>FlatSpec</code>, <code>WordSpec</code>, <code>fixture.FlatSpec</code>, and <code>fixture.WordSpec</code>
- * mix in <code>ShouldVerb</code> directly, and</li>
- * <li><code>ShouldMatchers</code> extends <code>ShouldVerb</code>, overriding the
- * <code>convertToStringShouldWrapper</code> implicit conversion function.</li>
- * </ol>
- *
- * <p>
- * So whether or not
- * a <code>FlatSpec</code>, <code>WordSpec</code>, <code>fixture.FlatSpec</code>, or <code>fixture.WordSpec</code>
- * mixes in <code>ShouldMatchers</code>, there will only be one
- * implicit conversion in scope that adds <code>should</code> methods to <code>String</code>s.
- * </p>
- *
- * </p>
- * Also, because the class of the result of the overriding <code>convertToStringShouldWrapper</code>
- * implicit conversion method provided in <code>ShouldMatchers</code> extends this trait's
- * <code>StringShouldWrapperForVerb</code> class, the four uses of <code>should</code> provided here
- * are still available. These four <code>should</code> are in fact available to any class
- * that mixes in <code>ShouldMatchers</code>, but each takes an implicit parameter that is provided
- * only in <code>FlatSpec</code> and <code>fixture.FlatSpec</code>, or <code>WordSpec</code> and
- * <code>fixture.WordSpec</code>.  
+ * <code>fixture.WordSpec</code>, is primarily for design symmetry with <code>ShouldVerb</code>
+ * and <code>MustVerb</code>. Both <code>ShouldVerb</code> and <code>MustVerb</code> must exist
+ * as a separate trait because an implicit conversion provided directly would conflict
+ * with the implicit conversion that provides <code>should</code> or <code>must</code> methods on <code>String</code>
+ * in the <code>ShouldMatchers</code> and <code>MustMatchers</code> traits.
  * </p>
  *
  * @author Bill Venners
  */
-trait ShouldVerb {
+trait CanVerb {
 
-  // This can't be final or abstract, because it is instantiated directly by the implicit conversion, and
-  // extended by something in ShouldMatchers.
+  // This one can be final, because it isn't extended by anything in the matchers DSL.
   /**
    * This class supports the syntax of <code>FlatSpec</code>, <code>WordSpec</code>, <code>fixture.FlatSpec</code>,
    * and <code>fixture.WordSpec</code>.
    *
    * <p>
-   * This class is used in conjunction with an implicit conversion to enable <code>should</code> methods to
+   * This class is used in conjunction with an implicit conversion to enable <code>can</code> methods to
    * be invoked on <code>String</code>s.
    * </p>
    *
    * @author Bill Venners
    */
-  class StringShouldWrapperForVerb(left: String) {
+  final class StringCanWrapperForVerb(left: String) {
 
     /**
      * Supports test registration in <code>FlatSpec</code> and <code>fixture.FlatSpec</code>.
@@ -125,7 +101,7 @@ trait ShouldVerb {
      * </p>
      *
      * <pre class="stHighlight">
-     * "A Stack (when empty)" should "be empty" in { ... }
+     * "A Stack (when empty)" can "be empty" in { ... }
      *                        ^
      * </pre>
      *
@@ -133,11 +109,11 @@ trait ShouldVerb {
      * <code>FlatSpec</code> passes in a function via the implicit parameter that takes
      * three strings and results in a <code>ResultOfStringPassedToVerb</code>. This method
      * simply invokes this function, passing in left, the verb string
-     * <code>"should"</code>, and right, and returns the result.
+     * <code>"can"</code>, and right, and returns the result.
      * </p>
      */
-    def should(right: String)(implicit fun: (String, String, String) => ResultOfStringPassedToVerb): ResultOfStringPassedToVerb = {
-      fun(left, "should", right)
+    def can(right: String)(implicit fun: (String, String, String) => ResultOfStringPassedToVerb): ResultOfStringPassedToVerb = {
+      fun(left, "can", right)
     }
 
     /**
@@ -149,7 +125,7 @@ trait ShouldVerb {
      * </p>
      *
      * <pre class="stHighlight">
-     * "A Stack (with one item)" should behave like nonEmptyStack(stackWithOneItem, lastValuePushed)
+     * "A Stack (with one item)" can behave like nonEmptyStack(stackWithOneItem, lastValuePushed)
      *                           ^
      * </pre>
      *
@@ -159,7 +135,7 @@ trait ShouldVerb {
      * simply invokes this function, passing in left, and returns the result.
      * </p>
      */
-    def should(right: BehaveWord)(implicit fun: (String) => BehaveWord): BehaveWord = {
+    def can(right: BehaveWord)(implicit fun: (String) => BehaveWord): BehaveWord = {
       fun(left)
     }
 
@@ -173,7 +149,7 @@ trait ShouldVerb {
      * </p>
      *
      * <pre class="stHighlight">
-     * "A Stack (when empty)" should { ...
+     * "A Stack (when empty)" can { ...
      *                        ^
      * </pre>
      *
@@ -181,12 +157,12 @@ trait ShouldVerb {
      * <code>WordSpec</code> passes in a function via the implicit parameter of type <code>StringVerbBlockRegistration</code>,
      * a function that takes two strings and a no-arg function and results in <code>Unit</code>. This method
      * simply invokes this function, passing in left, the verb string
-     * <code>"should"</code>, and the right by-name parameter transformed into a
+     * <code>"can"</code>, and the right by-name parameter transformed into a
      * no-arg function.
      * </p>
      */
-    def should(right: => Unit)(implicit fun: StringVerbBlockRegistration) {
-      fun(left, "should", right _)
+    def can(right: => Unit)(implicit fun: StringVerbBlockRegistration) {
+      fun(left, "can", right _)
     }
 
     /**
@@ -201,7 +177,7 @@ trait ShouldVerb {
      * <pre class="stHighlight">
      *    def provide = afterWord("provide")
      *
-     *   "The ScalaTest Matchers DSL" should provide {
+     *   "The ScalaTest Matchers DSL" can provide {
      *                                ^
      * </pre>
      *
@@ -209,17 +185,17 @@ trait ShouldVerb {
      * <code>WordSpec</code> passes in a function via the implicit parameter that takes
      * two strings and a <code>ResultOfAfterWordApplication</code> and results in <code>Unit</code>. This method
      * simply invokes this function, passing in left, the verb string
-     * <code>"should"</code>, and the <code>ResultOfAfterWordApplication</code> passed to <code>should</code>.
+     * <code>"can"</code>, and the <code>ResultOfAfterWordApplication</code> passed to <code>can</code>.
      * </p>
      */
-    def should(resultOfAfterWordApplication: ResultOfAfterWordApplication)(implicit fun: (String, String, ResultOfAfterWordApplication) => Unit) {
-      fun(left, "should", resultOfAfterWordApplication)
+    def can(resultOfAfterWordApplication: ResultOfAfterWordApplication)(implicit fun: (String, String, ResultOfAfterWordApplication) => Unit) {
+      fun(left, "can", resultOfAfterWordApplication)
     }
   }
 
   /**
-   * Implicitly converts an object of type <code>String</code> to a <code>StringShouldWrapperForVerb</code>,
-   * to enable <code>should</code> methods to be invokable on that object.
+   * Implicitly converts an object of type <code>String</code> to a <code>StringCanWrapper</code>,
+   * to enable <code>can</code> methods to be invokable on that object.
    */
-  implicit def convertToStringShouldWrapper(o: String): StringShouldWrapperForVerb = new StringShouldWrapperForVerb(o.trim)
+  implicit def convertToStringCanWrapper(o: String): StringCanWrapperForVerb = new StringCanWrapperForVerb(o.trim)
 }
