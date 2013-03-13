265524a1224c1b0bac4dc2d8f6ff5a5e45967df4
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2013-03-13 00:09:09 +0000

    Added TraversableEqualityConstraints. Fixed method name of the map implicit.
    git-svn-id: http://scalatest.googlecode.com/svn/trunk@5165 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalautils/TraversableEqualityConstraints.scala
+++ src/main/scala/org/scalautils/TraversableEqualityConstraints.scala
@@ -15,7 +15,5 @@
  */
 package org.scalautils
 
-trait MapEqualityConstraints {
-  implicit def mapEqualityConstraint[EAK, EAV, CA[_, _] <: collection.GenMap[_, _], EBK, EBV, CB[_, _] <: collection.GenMap[_, _]](implicit equalityOfA: Equality[CA[EAK, EAV]], evKey: EqualityConstraint[EAK, EBK], evValue: EqualityConstraint[EAV, EBV]): EqualityConstraint[CA[EAK, EAV], CB[EBK, EBV]] = new BasicEqualityConstraint[CA[EAK, EAV], CB[EBK, EBV]](equalityOfA)
-}
+trait TraversableEqualityConstraints extends SeqEqualityConstraints with SetEqualityConstraints with MapEqualityConstraints
 
